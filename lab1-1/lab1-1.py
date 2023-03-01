c93eb4455e164835381f8e21c437f0dbc6ae7ad7
WpythoN <45458018+WpythonW@users.noreply.github.com>
2023-03-02 02:32:10 +0300

    Update lab1-1.py

--- lab1-1/lab1-1.py
+++ lab1-1/lab1-1.py
@@ -1,74 +1,124 @@
-#LU Gauss
+#define _USE_MATH_DEFINES
 
-A = [ [2, -7, 8, -4],
-     [0, -1, 4, -1],
-     [3, -4, 2, -1],
-     [-9, 1, -4, 6] ]
-E = [ [1, 0, 0, 0],
-      [0, 1, 0, 0],
-      [0, 0, 1, 0],
-      [0, 0, 0, 1]]
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <iomanip>
 
-b = [57, 24, 18, 12]
+using namespace std;
 
-# A = [ [10, 1, 1],
-#       [2, 10, 1],
-#       [2, 2, 10]]
-# E = [ [1, 0, 0],
-#       [0, 1, 0],
-#       [0, 0, 1]]
-# b = [12, 13, 14]
+int n;
+
+void swap_rows(double** M, int row1, int row2) {
+    for (int i = 0; i < n; i++) {
+        swap(M[row1][i], M[row2][i]);
+    }
+}
+
+void print(double** M) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++) {
+            cout << round(M[i][j]*10000)/10000 << setw(5);
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void LUP(double** A, double** E, double* p, double* b) {
+    double det = 1;
+    int count = 0;
+    double* z = new double[n], * x = new double[n];
+    for (int i = 0; i < n; i++) {
+        z[i] = 0;
+        x[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        //поиск опорного элемента
+        double pivotValue = 0;
+        int pivot = -1;
+        for (int row = i; row < n; row++) {
+            if (fabs(A[row][i]) > pivotValue) {
+                pivotValue = fabs(A[row][i]);
+                pivot = row;
+            }
+        }
+        if (pivotValue != 0) {
+            //меняем местами i-ю строку и строку с опорным элементом
+            swap(p[pivot], p[i]);
+            //swap(b[pivot], b[i]);
+            swap_rows(A, pivot, i);
+            count++;
+        }
+
+        double elem = A[i][i];
+        double coeff;
+        for (int j = i + 1; j < n; j++) {
+            coeff = A[j][i] / elem;
+            for (int k = 0; k < n; k++) {
+                if (k >= i)
+                    A[j][k] = A[j][k] - A[i][k] * coeff; //Условие здесь, чтобы не залезать на матрицу L
+                E[j][k] = E[j][k] - E[i][k] * coeff;
+                A[j][i] = coeff;
+            }
+            
+        }
+        det *= A[i][i];
+    }
+    det *= pow(-1, count);
+
+    print(A);
+
+    for (int i = 0; i < n; i++) {
+        double s = 0;
+        for (int k = 0; k < i; k++)
+            s += z[k] * A[i][k];
+        z[i] = b[i] - s;
+        //cout << z[i] << " ";
+    }
+
+    for (int i = n - 1; i > -1; i--) {
+        double ui = A[i][i], s = 0;
+        for (int k = i + 1; k < n; k++)
+            s += x[k] * A[i][k];
+        x[i] = (z[i] - s) / ui;
+        cout << x[i] << " ";
+    }
+
+}
+
+int main() {
+    ifstream inFile;
+    inFile.open("matrix.txt");
+    inFile >> n;
+    double** A = new double* [n], **E = new double* [n];
+    double* b = new double[n], *p = new double[n];
+    for (int i = 0; i < n; i++) {
+        A[i] = new double[n];
+        E[i] = new double[n];
+        p[i] = i;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+        {
+            inFile >> A[i][j];
+            E[i][j] = 0;
+            cout << A[i][j] << " ";
+        }
+        E[i][i] = 1;
+        cout << "\n";
+    }
+
+    for (int i = 0; i < n; i++) {
+        inFile >> b[i];
+        cout << b[i] << " ";
+    }
+    cout << "\n\n";
+
+    LUP(A, E, p, b);
 
 
-z = [0] * len(b)
-x = [0] * len(b)
-n = len(A)
-for i in range(n):
-    elem = A[i][i]
-
-    for j in range(i+1, n):
-        coeff = A[j][i]/elem
-        for k in range(0, n):
-            if k >= i:
-                A[j][k] = A[j][k] - A[i][k] * coeff #Условие здесь, чтобы не залезать на матрицу L
-            E[j][k] = E[j][k] - E[i][k] * coeff
-        #A[j][i:] = [a-b for a, b in zip(A[j][i:], [t * coeff for t in A[i][i:]])]
-        A[j][i] = coeff
-
-det = 1
-for i in range(n):
-    det *= A[i][i]
-
-
-
-for i in range(n):
-    s = 0
-    for k in range(0, i):
-        s += z[k]*A[i][k]
-    z[i] = b[i] - s
-    #z.append(b[i] - sum([z[k]*A[i][k] for k in range(0, i)]))
-
-for i in range(n-1, -1, -1):
-    ui = A[i][i]
-    s = 0
-    for k in range(i+1, n):
-        s += x[k] * A[i][k]
-    x[i] = (z[i] - s) / ui
-    #x.insert(0, (z[i] - sum([x[k]*A[i][k] for k in range(i+1, n)]))/ui)
-
-print("X =", x)
-print("Det =", det)
-if det > 0:
-    print("Det > 0 => reversable")
-
-for i in range(n):
-    for j in range(n-1, -1, -1):
-        ui = A[j][j]
-        s = 0
-        for k in range(j+1, n):
-            s += E[k][i] * A[j][k]
-        E[j][i] = round((E[j][i] - s) / ui, 4)
-
-print("reverse A matrix:")
-for i in E:
-    print(i)
+}
